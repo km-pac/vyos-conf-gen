5ace24d310b54e0d5b1b83cf706e940f6378129c
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 10:17:39 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -18,16 +18,18 @@ do
   read -p "Set $key to: " sys_value
   sys_conf[$key]=$sys_value
   echo "Successfully set $key to ${sys_conf[$key]}"
-  # if [$key == "sys_ntp"]
-  # then
-  #   read -p "Do you want to set the NTP Server? [y/n] " response
-  #   if [$response == "y"]
-  #   then
-  #     echo "Skipping NTP Server setup"
-  #   read -p "Set $key to: " ntp_value
-  #   sys_conf[$key] = ntp_value
-  #   fi
-  # fi
+  
+  if [$key == "sys_ntp"]
+  then
+    echo "SYS NTP HERE"
+    # read -p "Do you want to set the NTP Server? [y/n] " response
+    # if [$response == "y"]
+    # then
+    #   echo "Skipping NTP Server setup"
+    # read -p "Set $key to: " ntp_value
+    # sys_conf[$key] = ntp_value
+    # fi
+  fi
   
 done
 
