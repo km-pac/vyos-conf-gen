62d96be4a901f35f2f243c61abb8985132da6849
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 13:17:53 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -40,13 +40,13 @@ declare -a snmp_conf_key=("snmp_community" "snmp_contact" "snmp_location")
 # done
 
 #loop through different snmp configuration
-# echo "\nCONFIGURING SNMP SERVICES"
+# echo -e "\nCONFIGURING SNMP SERVICES"
 # while [[ $snmp_done != "y" ]]
 # do
 #   declare -a snmp_conf_values=()
 #   for snmpkey in ${snmp_conf_key[@]}; do
-#     read -p "Set $snmpkey: " snmp_values
-#     snmp_conf_values+=("$snmp_values")
+#     read -p "Set $snmpkey: " snmp_value
+#     snmp_conf_values+=("$snmp_value")
 #   done
 #   #outputs the set system configuration
 #   for index in "${!snmp_conf_key[@]}"; do
@@ -56,6 +56,42 @@ declare -a snmp_conf_key=("snmp_community" "snmp_contact" "snmp_location")
 #   done
 #   #confirm if satisfied with the configuration set
 #   read -p "Are you satisfied with this SNMP configuration? [y/n]: " snmp_done
+# done
+
+# echo -e "\nCONFIGURING SNMP SERVICES"
+while [[ $snmp_done != "y" ]]
+do
+  declare -a snmp_conf=()
+  for snmpkey in ${!snmp_conf[@]}; do
+    read -p "Set $snmpkey: " snmp_value
+    snmp_conf[$snmpkey]=$snmp_value
+  done
+  #outputs the set system configuration
+  for snmpkey in "${!snmp_conf[@]}"; do
+      snmp_key="$snmpkey"
+      snmp_values="${snmp_conf_values[$snmpkey]}"
+      echo "$snmp_key : $snmp_values"
+  done
+  #confirm if satisfied with the configuration set
+  read -p "Are you satisfied with this SNMP configuration? [y/n]: " snmp_done
+done
+
+#loop through interface configuration
+# echo -e "\nCONFIGURING INTERFACES"
+# while [[ ipv4_done != 'y' ]]
+# do
+#   declare -a int_ipv4_values=()
+#   read -p "Number of interafaces: " interface_count
+#   for ((int = 0; int < interface_count; int++)); do
+#     read -p "IP Address for eth$int: " ipv4_value
+#     int_ipv4_values+=("$ipv4_value")
+#   done
+#   for index in "${!int_ipv4_values[@]}"; do
+#       interface="eth$index"
+#       ipv4_address="${int_ipv4_values[$index]}"
+#       echo "$interface : $ipv4_address"
+#   done
+#   read -p "Are you satisfied with this IPv4 configuration? [y/n]: " ipv4_done
 # done
 
 # cat << EOF > config.txt
@@ -70,23 +106,7 @@ declare -a snmp_conf_key=("snmp_community" "snmp_contact" "snmp_location")
 # set snmp location ${snmp_conf_values[2]}
 # EOF
 
-#loop through interface configuration
-echo -e "\nCONFIGURING INTERFACES"
-while [[ ipv4_done != 'y' ]]
-do
-  declare -a int_ipv4_values=()
-  read -p "Number of interafaces: " interface_count
-  for ((int = 0; int < interface_count; int++)); do
-    read -p "IP Address for eth$int: " ipv4_value
-    int_ipv4_values+=("$ipv4_value")
-  done
-  for index in "${!int_ipv4_values[@]}"; do
-      interface="eth$index"
-      ipv4_address="${int_ipv4_values[$index]}"
-      echo "$interface : $ipv4_address"
-  done
-  read -p "Are you satisfied with this IPv4 configuration? [y/n]: " ipv4_done
-done
+
 
 
 
