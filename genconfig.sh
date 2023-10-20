ac8bb5bfccdddd195d6c9da1ff47256570e2b2f1
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 10:52:35 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -5,29 +5,20 @@
 #This is where the config will be saved
 touch config.txt
 
-
-#sys_conf_array=array([sys_hostname], [sys_timezone], [sys_ntp], [sys_banner])
-#snmp_conf_array = (snmp_community, snmp_contact, snmp_location)
-
 #array of system and snmp configuration
 declare -a sys_conf_keys=("sys_hostname" "sys_timezone" "sys_banner" "sys_ntp")
 declare -a snmp_conf_keys=("snmp_community" "snmp_contact" "sys_banner" "sys_ntp")
 
-# for key in ${sys_conf_keys[@]};
-# do
-#   read -p "Set $key to: " sys_value
-#   sys_conf[$key]=$sys_value
-#   echo "Successfully set $key to ${sys_conf[$key]}"
-#   if ["$key" -eq "sys_ntp"];
-#   then
-#     echo "SYS NTP key here"
-#   fi
-# done
-read text
-if [[ "$text" == "test" ]]
-then
-  echo "The variable is test"
-fi
+for key in ${sys_conf_keys[@]};
+do
+  read -p "Set $key to: " sys_value
+  sys_conf[$key]=$sys_value
+  echo "Successfully set $key to ${sys_conf[$key]}"
+  if [[ "$key" == "sys_ntp" ]]
+  then
+    echo "SYS NTP key here"
+  fi
+done
 
   # if ["$key"=="sys_ntp"];
   # then
