266c4bd4c9486db78f27b43741b112353bbdfaf6
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 11:51:58 +0800

    Done with system config

--- genconfig.sh
+++ genconfig.sh
@@ -10,27 +10,31 @@ declare -a sys_conf_key=("sys_hostname" "sys_timezone" "sys_banner" "sys_ntp")
 declare -a snmp_conf=("snmp_community" "snmp_contact" "snmp_location")
 
 #loop through different system configuration
-for key in ${sys_conf_key[@]}; do
-  #sys_ntp config is optional;
-  if [[ $key == "sys_ntp" ]]
-  then
-      read -p "Do you want to set the NTP Server? [y/n] " response
-      if [[ $response == "n" ]]
-      then
-        echo "Skipping NTP Server setup"
-        break
-      fi
-  fi
-  read -p "Set $key to: " sys_value
-  sys_conf_value+=($sys_value)
-  echo "Successfully set $key to ${sys_conf_value[$key]}"
-done
-
-#outputs the set system configuration
-for index in "${!sys_conf_key[@]}"; do
-    key="${sys_conf_key[$index]}"
-    value="${sys_conf_value[$index]}"
-    echo "$key : $value"
+while [[ $sys_done != "y" ]]
+do
+  for key in ${sys_conf_key[@]}; do
+    #sys_ntp config is optional;
+    if [[ $key == "sys_ntp" ]]
+    then
+        read -p "Do you want to set the NTP Server? [y/n]: " response
+        if [[ $response == "n" ]]
+        then
+          echo "Skipping NTP Server setup"
+          break
+        fi
+    fi
+    read -p "Set $key to: " sys_value
+    sys_conf_value+=($sys_value)
+    echo "Successfully set $key to ${sys_conf_value[$key]}"
+  done
+  #outputs the set system configuration
+  for index in "${!sys_conf_key[@]}"; do
+      key="${sys_conf_key[$index]}"
+      value="${sys_conf_value[$index]}"
+      echo "$key : $value"
+  done
+  #confirm if satisfied with the configuration set
+  read -p "Are you satisfied with this configuration? [y/n]: " sys_done
 done
 
 
