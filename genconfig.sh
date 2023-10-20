c299571e19024fa264a572dcec77e564b79571eb
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 12:10:48 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -13,9 +13,9 @@ declare -a snmp_conf=("snmp_community" "snmp_contact" "snmp_location")
 while [[ $sys_done != "y" ]]
 do
   declare -a sys_conf_value=()
-  for key in ${sys_conf_key[@]}; do
+  for syskey in ${sys_conf_key[@]}; do
     #sys_ntp config is optional;
-    if [[ $key == "sys_ntp" ]]
+    if [[ $syskey == "sys_ntp" ]]
     then
         read -p "Do you want to set the NTP Server? [y/n]: " response
         if [[ $response == "n" ]]
@@ -24,26 +24,49 @@ do
           break
         fi
     fi
-    read -p "Set $key to: " sys_value
+    read -p "Set $syskey to: " sys_value
     sys_conf_value+=($sys_value)
-    echo "Successfully set $key to $sys_value"
+    echo "Successfully set $syskey to $sys_value"
   done
   #outputs the set system configuration
   for index in "${!sys_conf_key[@]}"; do
-      key="${sys_conf_key[$index]}"
-      value="${sys_conf_value[$index]}"
-      echo "$key : $value"
+      sys_key="${sys_conf_key[$index]}"
+      sys_value="${sys_conf_value[$index]}"
+      echo "$sys_key : $sys_value"
   done
   #confirm if satisfied with the configuration set
   read -p "Are you satisfied with this configuration? [y/n]: " sys_done
 done
 
-cat << EOF > config.txt
+#loop through different system configuration
+while [[ $snmp_done != "y" ]]
+do
+  declare -a snmp_conf_value=()
+  for snmpkey in ${snmp_conf_key[@]}; do
+    read -p "Set $snmpkey to: " snmp_value
+    snmp_conf_value+=($snmp_value)
+    echo "Successfully set $snmpkey to $snmp_value"
+  done
+  #outputs the set system configuration
+  for index in "${!sys_conf_key[@]}"; do
+      snmp_key="${sys_conf_key[$index]}"
+      snmp_value="${sys_conf_value[$index]}"
+      echo "$snmp_key : $snmp_value"
+  done
+  #confirm if satisfied with the configuration set
+  read -p "Are you satisfied with this configuration? [y/n]: " snmp_done
+done
+
+cat << EOF >> config.txt
 set system host-name ${sys_conf_value[0]}
 set system time-zone ${sys_conf_value[1]}
 set system login banner post-login ${sys_conf_value[2]}
 set system login banner pre-login ${sys_conf_value[2]}
 set system ntp server ${sys_conf_value[3]}
+
+set snmp community ${snmp_conf_value[0]}
+set snmp contact ${snmp_conf_value[1]}
+set snmp location ${snmp_conf_value[2]}
 EOF
 
 
