6c128dc7c99b648134bd8b6f06c55f40b845cc85
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 11:30:10 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -6,11 +6,11 @@
 touch config.txt
 
 #array of system and snmp configuration
-declare -a sys_conf=(["sys_hostname"]="", ["sys_timezone"]="", ["sys_banner"]="", ["sys_ntp"]="")
+declare -a sys_conf_key=("sys_hostname" "sys_timezone" "sys_banner" "sys_ntp")
 declare -a snmp_conf=("snmp_community" "snmp_contact" "sys_banner" "sys_ntp")
 
 #loop through different system configuration
-for key in ${sys_conf[@]}; do
+for key in ${sys_conf_key[@]}; do
   #sys_ntp config is optional;
   if [[ $key == "sys_ntp" ]]
   then
@@ -22,7 +22,7 @@ for key in ${sys_conf[@]}; do
       fi
   fi
   read -p "Set $key to: " sys_value
-  sys_conf[$key]=$sys_value
+  sys_conf_value[$key]+=($sys_value)
   echo "Successfully set $key to ${sys_conf_value[$key]}"
 done
 
