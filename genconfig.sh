3ff7f26f69bb7971f8956b644c24c42fea2c06ef
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 10:58:38 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -11,13 +11,21 @@ declare -a snmp_conf_keys=("snmp_community" "snmp_contact" "sys_banner" "sys_ntp
 
 for key in ${sys_conf_keys[@]};
 do
+  if [[ $key == "sys_ntp" ]] 
+  then
+      read -p "Do you want to set the NTP Server? [y/n] " response
+      if [[ $response == "y" ]] 
+      then
+        echo "Skipping NTP Server setup"
+      # else
+      #   read -p "Set $key to: " sys_value
+      #   sys_conf[$key]=$sys_value
+      #   echo "Successfully set $key to ${sys_conf[$key]}"
+      fi
+  fi
   read -p "Set $key to: " sys_value
   sys_conf[$key]=$sys_value
   echo "Successfully set $key to ${sys_conf[$key]}"
-  if [[ "$key" == "sys_ntp" ]]
-  then
-    echo "SYS NTP key here"
-  fi
 done
 
   # if ["$key"=="sys_ntp"];
