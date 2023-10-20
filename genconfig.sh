5f753aad66bab5c72bef910908c6cb5e805ced99
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-20 11:31:03 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -22,7 +22,7 @@ for key in ${sys_conf_key[@]}; do
       fi
   fi
   read -p "Set $key to: " sys_value
-  sys_conf_value += ($sys_value)
+  sys_conf_value+=($sys_value)
   echo "Successfully set $key to ${sys_conf_value[$key]}"
 done
 
