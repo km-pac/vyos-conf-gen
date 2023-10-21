1f1feeb1b11992d3c9aa04f8ce1ab27ac3427709
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-21 10:57:07 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -98,7 +98,7 @@ do
             read -p "Set IP Address for VIF $vif_id: " first_octet subnet_mask
             ipv4_values+=("eth$int vif $vif_id address $first_octet.168.$vif_id.1/$subnet_mask")
           fi
-          (( ${int}_count++ ))
+          (( "${int}_count"++ ))
           read -p "Do you want to ADD another VIF? [y/n]: " vif_add_done
           printf "\n"
         done
