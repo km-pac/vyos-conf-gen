556b5f93d9d85ff3bfe347686b7f7b7c121d3564
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-23 11:07:19 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -226,7 +226,7 @@ do
 
     for index in "${!temp_rule_nums[@]}"; do
       # rule_nums+=("${temp_rule_nums[$index]}")
-      echo "${temp_rule_nums[$index]}"
+      rule_nums+="${temp_rule_nums[$index]}"
     done
 
     # for key in "${!rule_nums[@]}"; do
