108fbd0dade22cb7f4e60d3ef7c7ae97ac673202
KylePac <147895229+km-pac@users.noreply.github.com>
2023-10-23 08:40:56 +0800

    Update genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -203,22 +203,26 @@ do
   do
     while [[ rule_num_add_done != 'n' ]]
     do
-      declare -a temp_rule_nums=()
+      while [[ rule_num_done != 'y' ]]
+      do
+        declare -a temp_rule_nums=()
+        
+        read -p "Set ${cyan_style}Rule Number${reset_style}: " rule_num
+        read -p "Set ${cyan_style}Protocol${reset_style} for ${highlight_style}$rule_num${reset_style}: " protocol
+        read -p "Set ${cyan_style}Source Address${reset_style} for ${highlight_style}$rule_num${reset_style}: " src_address
+        read -p "Set ${cyan_style}Destination Address${reset_style} for ${highlight_style}$rule_num${reset_style}: " dest_address
+        read -p "Set ${cyan_style}Destination Port Number${reset_style} for ${highlight_style}$rule_num${reset_style}: " port_num
+        
+        temp_rule_nums["$rule_num"]+="$protocol $src_address $dest_address $port_num"
+        
+        for key in "${!temp_rule_nums[@]}"; do
+          echo "Key: $key"
+          echo "Value: ${temp_rule_nums[$key]}"
+        done
       
-      read -p "Set ${cyan_style}Rule Number${reset_style}: " rule_num
-      read -p "Set ${cyan_style}Protocol${reset_style} for ${highlight_style}$rule_num${reset_style}: " protocol
-      read -p "Set ${cyan_style}Source Address${reset_style} for ${highlight_style}$rule_num${reset_style}: " src_address
-      read -p "Set ${cyan_style}Destination Address${reset_style} for ${highlight_style}$rule_num${reset_style}: " dest_address
-      read -p "Set ${cyan_style}Destination Port Number${reset_style} for ${highlight_style}$rule_num${reset_style}: " port_num
-      
-      temp_rule_nums["$rule_num"]+="$protocol $src_address $dest_address $port_num"
-      
-      for key in "${!temp_rule_nums[@]}"; do
-        echo "Key: $key"
-        echo "Value: ${temp_rule_nums[$key]}"
+      read -p "Are you satisfied with this rule number? [y/n]: " rule_num_done
       done
-      
-      read -p "Are you satisfied with this rule number? [y/n]: " rule_num_add_done
+    read -p "Do you want to add more rules to FIREWALL_NAME? [y/n]: " rule_num_add_done
     done
     
     for index in "${!temp_rule_nums[@]}"; do
